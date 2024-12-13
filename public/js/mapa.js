c7ee915325c7d7d85982af4878f173a8f7ddf788
Angel Jesus <230592@utxicotepec.edu.mx>
2024-12-13 13:10:22 -0600

    actividad

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,42 +1,14 @@
-(function () {
-
-  const lat = document.querySelector('#lat').value || 20.2741536;  // Coordenadas de tu casa
-  const lng = document.querySelector('#lng').value || -97.9598598; // Coordenadas de tu casa
-  const mapa = L.map('mapa').setView([lat, lng], 16);
-  let marker;
-
-  // Utilizar provider y geocoder
-  const geocodeService = L.esri.Geocoding.geocodeService();
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(mapa);
-
-  // El pin
-  marker = new L.marker([lat, lng], {
-      draggable: true,
-      autoPan: true
-  })
-  .addTo(mapa)
-
-  // Detectar el movimiento del pin
-  marker.on('moveend', function (e) {
-      marker = e.target
-
-      const posicion = marker.getLatLng();
-
-      mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
-
-      // Obtener información de las calles al soltar el pin
-      geocodeService.reverse().latlng(posicion, 13).run(function (error, resultado) {
-          marker.bindPopup(resultado.address.LongLabel)
-
-          // Llenar los campos
-          document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
-          document.querySelector('#calle').value = resultado?.address?.Address ?? '';
-          document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
-          document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
-      })
-  })
-
-})()
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
+/******/ })()
+;
